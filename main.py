34b6e4bc783d9b546dcadb3486172822312a7905
Andrew <66111825+AndrewDisco@users.noreply.github.com>
2023-12-23 23:22:48 +0200

    Better syntax highlighting & remove IO files

--- main.py
+++ main.py
@@ -1,24 +1,154 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit, QLabel, QLineEdit, QPushButton, QFileDialog, QMessageBox, QTextEdit, QTableWidget, QTableWidgetItem, QHeaderView, QHBoxLayout
-from PyQt5.QtGui import QSyntaxHighlighter, QTextCharFormat, QTextCursor, QTextBlockUserData, QColor, QFont
-from PyQt5.QtCore import Qt, pyqtSlot, QRegularExpression
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit, QLabel, QLineEdit, QPushButton, QFileDialog, QMessageBox, QTextEdit, QTableWidget, QTableWidgetItem, QHeaderView, QHBoxLayout, QPlainTextEdit
+from PyQt5.QtGui import QSyntaxHighlighter, QTextCharFormat, QTextCursor, QTextBlockUserData, QColor, QFont, QPainter, QTextFormat
+from PyQt5.QtCore import Qt, pyqtSlot, QRegularExpression, QRect
 from PyQt5 import QtGui
 import subprocess
 import time
 import os
 import qdarktheme
-import ctypes
+ 
+class QCodeEditor(QPlainTextEdit):
+    class NumberBar(QWidget):
+        def __init__(self, editor):
+            QWidget.__init__(self, editor)
+            
+            self.editor = editor
+            self.editor.blockCountChanged.connect(self.updateWidth)
+            self.editor.updateRequest.connect(self.updateContents)
+            self.font = QFont()
+            self.numberBarColor = QColor("#49494A")
+                     
+        def paintEvent(self, event):
+            
+            painter = QPainter(self)
+            painter.fillRect(event.rect(), self.numberBarColor)
+             
+            block = self.editor.firstVisibleBlock()
+ 
+            # Iterate over all visible text blocks in the document.
+            while block.isValid():
+                blockNumber = block.blockNumber()
+                block_top = int(self.editor.blockBoundingGeometry(block).translated(self.editor.contentOffset()).top())
+ 
+                # Check if the position of the block is out side of the visible area.
+                if not block.isVisible() or block_top >= event.rect().bottom():
+                    break
+ 
+                # We want the line number for the selected line to be bold.
+                if blockNumber == self.editor.textCursor().blockNumber():
+                    self.font.setBold(True)
+                    painter.setPen(QColor("#2D74E6"))
+                else:
+                    self.font.setBold(False)
+                    painter.setPen(QColor("#8ab4f7"))
+                painter.setFont(self.font)
+                
+                # Draw the line number right justified at the position of the line.
+                paint_rect = QRect(0, block_top, self.width(), self.editor.fontMetrics().height())
+                painter.drawText(paint_rect, Qt.AlignRight, str(blockNumber+1))
+ 
+                block = block.next()
+ 
+            painter.end()
+            
+            QWidget.paintEvent(self, event)
+ 
+        def getWidth(self):
+            count = self.editor.blockCount()
+            width = self.fontMetrics().width(str(count)) + 8
+            return width      
+        
+        def updateWidth(self):
+            width = self.getWidth()
+            if self.width() != width:
+                self.setFixedWidth(width)
+                self.editor.setViewportMargins(width, 0, 0, 0);
+ 
+        def updateContents(self, rect, scroll):
+            if scroll:
+                self.scroll(0, scroll)
+            else:
+                self.update(0, rect.y(), self.width(), rect.height())
+            
+            if rect.contains(self.editor.viewport().rect()):   
+                fontSize = self.editor.currentCharFormat().font().pointSize()
+                self.font.setPointSize(fontSize)
+                self.font.setStyle(QFont.StyleNormal)
+                self.updateWidth()
+                
+        
+    def __init__(self, DISPLAY_LINE_NUMBERS=True, HIGHLIGHT_CURRENT_LINE=True,
+                 SyntaxHighlighter=None, *args):        
+        '''
+        Parameters
+        ----------
+        DISPLAY_LINE_NUMBERS : bool 
+            switch on/off the presence of the lines number bar
+        HIGHLIGHT_CURRENT_LINE : bool
+            switch on/off the current line highliting
+        SyntaxHighlighter : QSyntaxHighlighter
+            should be inherited from QSyntaxHighlighter
+        
+        '''                  
+        super(QCodeEditor, self).__init__()
+        self.setLineWrapMode(QPlainTextEdit.NoWrap)
+                               
+        self.DISPLAY_LINE_NUMBERS = DISPLAY_LINE_NUMBERS
+
+        if DISPLAY_LINE_NUMBERS:
+            self.number_bar = self.NumberBar(self)
+            self.setViewportMargins(self.number_bar.getWidth(), 0, 0, 0)
+            
+        if HIGHLIGHT_CURRENT_LINE:
+            self.currentLineNumber = None
+            self.currentLineColor = QColor("#333539")
+            self.cursorPositionChanged.connect(self.highligtCurrentLine)
+        
+        if SyntaxHighlighter is not None: # add highlighter to textdocument
+           self.highlighter = SyntaxHighlighter(self.document())         
+                 
+    def resizeEvent(self, *e):
+        '''overload resizeEvent handler'''
+                
+        if self.DISPLAY_LINE_NUMBERS:   # resize number_bar widget
+            cr = self.contentsRect()
+            rec = QRect(cr.left(), cr.top(), self.number_bar.getWidth(), cr.height())
+            self.number_bar.setGeometry(rec)
+        
+        QPlainTextEdit.resizeEvent(self, *e)
+
+    def highligtCurrentLine(self):
+        newCurrentLineNumber = self.textCursor().blockNumber()
+        if newCurrentLineNumber != self.currentLineNumber:                
+            self.currentLineNumber = newCurrentLineNumber
+            hi_selection = QTextEdit.ExtraSelection() 
+            hi_selection.format.setBackground(self.currentLineColor)
+            hi_selection.format.setProperty(QTextFormat.FullWidthSelection, True)
+            hi_selection.cursor = self.textCursor()
+            hi_selection.cursor.clearSelection() 
+            self.setExtraSelections([hi_selection])        
+
 
 class CppSyntaxHighlighter(QSyntaxHighlighter):
     def __init__(self, parent=None):
         super(CppSyntaxHighlighter, self).__init__(parent)
-        
+
         self.keywordFormat = QTextCharFormat()
         self.keywordFormat.setForeground(QColor("#569CD6"))
         self.keywordFormat.setFontWeight(QFont.Bold)
-        
+
         self.operatorFormat = QTextCharFormat()
-        self.operatorFormat.setForeground(QColor("#D4D4D4"))
+        self.operatorFormat.setForeground(QColor("#e8e2b7"))
+
+        self.numberFormat = QTextCharFormat()
+        self.numberFormat.setForeground(QColor("#B5CEA8"))
+
+        self.stringFormat = QTextCharFormat()
+        self.stringFormat.setForeground(QColor("#CE9178"))
+
+        self.directiveFormat = QTextCharFormat()
+        self.directiveFormat.setForeground(QColor("#D4D4D4"))
 
         keywords = [
             "alignas", "alignof", "and", "and_eq", "asm", "auto", "bitand", "bitor",
@@ -34,12 +164,20 @@ class CppSyntaxHighlighter(QSyntaxHighlighter):
             "typename", "union", "unsigned", "using", "virtual", "void", "volatile",
             "wchar_t", "while", "xor", "xor_eq"
         ]
+        
+        self.rules = [
+            (r'\b%s\b' % keyword, 0, self.keywordFormat) for keyword in keywords
+        ] + [
+            (r'(\+|\-|\*|\/|\%|==|!=|<|>|<=|>=|&&|\|\||\!|\?|\:|\=|\+=|\-=|\*=|\/=|\%=|\&=|\|=|\^=|<<|>>|<<=|>>=|\(|\)|\{|\}|\[|\])', 0, self.operatorFormat),
+            (r'\b\d+\b', 0, self.numberFormat),  # Match numbers
+            (r'"[^"]*"', 0, self.stringFormat),   # Match strings
+            (r'#\w+', 0, self.directiveFormat),   # Match preprocessor directives
+        ]
 
-        self.rules = [(r'\b%s\b' % keyword, 0, self.keywordFormat) for keyword in keywords]
-        operators = r'(\+|\-|\*|\/|\%|==|!=|<|>|<=|>=|&&|\|\||\!|\?|\:|\=|\+=|\-=|\*=|\/=|\%=|\&=|\|=|\^=|<<|>>|<<=|>>=|\(|\)|\{|\}|\[|\])'
-        self.rules.append((operators, 0, self.operatorFormat))
         self.multiLineCommentFormat = QTextCharFormat()
         self.multiLineCommentFormat.setForeground(QColor("#57A64A"))
+
+        
 
     def highlightBlock(self, text):
         for pattern, nth, format in self.rules:
@@ -48,6 +186,7 @@ class CppSyntaxHighlighter(QSyntaxHighlighter):
             while match_iterator.hasNext():
                 match = match_iterator.next()
                 self.setFormat(match.capturedStart(), match.capturedLength(), format)
+
         self.setCurrentBlockUserData(None)
         self.matchMultilineComment(text, self.multiLineCommentFormat)
 
@@ -55,10 +194,10 @@ class CppSyntaxHighlighter(QSyntaxHighlighter):
         startIndex = 0
         if self.previousBlockState() > 0:
             startIndex = self.previousBlockState()
-    
+
         if startIndex == -1:
             startIndex = 0
-    
+
         commentStart = text.find("/*", startIndex)
         while commentStart >= 0:
             commentEnd = text.find("*/", commentStart)
@@ -70,7 +209,16 @@ class CppSyntaxHighlighter(QSyntaxHighlighter):
                 self.setFormat(commentStart, commentLength, format)
                 commentStart = text.find("/*", commentEnd + 2)
 
-
+    def highlightCurrentLine(self):
+        newCurrentLineNumber = self.textCursor().blockNumber()
+        if newCurrentLineNumber != self.currentLineNumber:
+            self.currentLineNumber = newCurrentLineNumber
+            hi_selection = QTextEdit.ExtraSelection()
+            hi_selection.format.setBackground(self.currentLineColor)
+            hi_selection.format.setProperty(QTextFormat.FullWidthSelection, True)
+            hi_selection.cursor = self.textCursor()
+            hi_selection.cursor.clearSelection()
+            self.setExtraSelections([hi_selection])
 
     class CommentUserData(QTextBlockUserData):
         def __init__(self, endingIndex):
@@ -95,12 +243,20 @@ class CPPCheckerApp(QWidget):
         # icon
         self.setWindowIcon(QtGui.QIcon('icon.png'))
 
-        self.code_text = QTextEdit(self)
+        self.code_text = QCodeEditor(self)
         layout.addWidget(self.code_text)
+
+        cppButtons = QHBoxLayout()
 
         browse_cpp_button = QPushButton("Browse CPP", self)
         browse_cpp_button.clicked.connect(self.browseCPP)
-        layout.addWidget(browse_cpp_button)
+        cppButtons.addWidget(browse_cpp_button)
+
+        self.remove_io_button = QPushButton("Remove IO", self)
+        self.remove_io_button.clicked.connect(self.removeIO)
+        cppButtons.addWidget(self.remove_io_button)
+
+        layout.addLayout(cppButtons)
 
         mingw_label = QLabel("Mingw Path:", self)
         layout.addWidget(mingw_label)
@@ -152,7 +308,7 @@ class CPPCheckerApp(QWidget):
 
         self.setLayout(layout)
         self.setGeometry(100, 100, 600, 400)
-        self.setWindowTitle('CPP Checker')
+        self.setWindowTitle('Judgee 0.3')
         self.show()
 
     @pyqtSlot()
@@ -231,7 +387,7 @@ class CPPCheckerApp(QWidget):
             if file.endswith(".in"):
                 test_number = file.split('.')[0]
                 in_file = f'{folder_path}/{test_number}.in'
-                out_file = f'{folder_path}/{test_number}.ok'
+                out_file = f'{folder_path}/{test_number}.out'
 
                 # Run the compiled executable with the input file & get the output
                 command = f'temp.exe < "{in_file}" > temp.out'
@@ -318,9 +474,34 @@ class CPPCheckerApp(QWidget):
     def showResult(self, result, time_taken):
         QMessageBox.information(self, "Result", f'Test {result}! Time Taken: {time_taken}')
 
+    @pyqtSlot()
+    def removeIO(self):
+        cpp_code = self.code_text.toPlainText()
+        lines = cpp_code.split('\n')
+        read_line = ""
+        output_line = ""
+        read_variable = ""
+        output_variable = ""
+
+        for line in lines:
+            if "ifstream" in line:
+                read_line = line
+                read_variable = line.split(' ')[1].split('(')[0]
+            if "ofstream" in line:
+                output_line = line
+                output_variable = line.split(' ')[1].split('(')[0]
+            
+        if read_line == "" or output_line == "":
+            self.showError("Error: Could not find input/output lines. \nDoes your code use ifstream/ofstream?")
+            return
+        
+        cpp_code = cpp_code.replace(read_line, "")
+        cpp_code = cpp_code.replace(output_line, "")
+        cpp_code = f'#define cin {read_variable}\n#define cout {output_variable}\n' + cpp_code
+        self.code_text.setPlainText(cpp_code)
+        self.highlighter = CppSyntaxHighlighter(self.code_text.document())
+
 if __name__ == '__main__':
-    myappid = 'mycompany.myproduct.subproduct.version' 
-    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
     qdarktheme.enable_hi_dpi()
     app = QApplication(sys.argv)
     qdarktheme.setup_theme()
